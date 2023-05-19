ece8c6828db208c7c821eb8df70345b9beeeec2a
Ceaile <ceaile1994@protonmail.com>
2023-05-19 17:11:21 +0200

    Añadido el ejemplo de ayer de interfaces graficas

---
+++ PROGRAMACION/ejercicios/src/main/java/ejerciciosInterfacesGraficas/interfacesGraf01.java
@@ -0,0 +1,22 @@
+/*
+Aplicación gráfica que permita introducir un número entero y luego saber
+si dicho número es par o impar. Utiliza un JTextField para introducir el valor, un
+JButton con el texto “¿par o impar?” y un JLabel para mostrar “PAR” o “IMPAR”
+según el caso.
+ */
+package ejerciciosInterfacesGraficas;
+
+/**
+ *
+ * @author DAW
+ */
+public class interfacesGraf01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
